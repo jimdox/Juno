db5dc6ed8279877e6e8517b62b4f3bae45e070ca
jimdox <jdoxtader@protonmail.com>
2020-01-10 17:46:45 -0500

    codebase refactor fix

--- bx_engine/lib/spdlog/out/build/x64-Debug/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/dev/BlackBox_VS/BlackBox_Engine/lib/spdlog/tests
-# Build directory: C:/dev/BlackBox_VS/BlackBox_Engine/lib/spdlog/out/build/x64-Debug/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(spdlog-utests "C:/dev/BlackBox_VS/BlackBox_Engine/lib/spdlog/out/build/x64-Debug/tests/spdlog-utests.exe")
-set_tests_properties(spdlog-utests PROPERTIES  _BACKTRACE_TRIPLES "C:/dev/BlackBox_VS/BlackBox_Engine/lib/spdlog/tests/CMakeLists.txt;29;add_test;C:/dev/BlackBox_VS/BlackBox_Engine/lib/spdlog/tests/CMakeLists.txt;0;")
